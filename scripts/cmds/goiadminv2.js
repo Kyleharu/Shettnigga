9a5c60ff44ee2284add79d7721055589a42a85b3
Secretaker34 <kyleteker@gmail.com>
2024-05-26 13:22:02 +0800

    Update goiadminv2.js

--- scripts/cmds/goiadminv2.js
+++ scripts/cmds/goiadminv2.js
@@ -17,13 +17,13 @@ module.exports = {
 			"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
 			"busy pa ata yun kaya mag-antay ka",
 			"Sorry, naka bebetime pa don't disturb him 🙄",
-			"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/swordigo.swordslush 😏",
+			"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/itssmekylebaitit 😏",
 			"Another tag in my Creator, i will kick your fucking ass"
 		];
 
-		const CliffRegex = /^(@Cliff Vincent V. Torrevillas 󱢏)$/i;
+		const CliffRegex = /^(@Kyle Bait-it)$/i;
 		if (event.body && CliffRegex.test(event.body)) {
-			api.setMessageReaction("😍", event.messageID, (err) => {}, true);
+			api.setMessageReaction("🤨", event.messageID, (err) => {}, true);
 			return api.sendMessage({ body: msg[Math.floor(Math.random() * msg.length)] }, event.threadID, event.messageID);
 		}
 	},
