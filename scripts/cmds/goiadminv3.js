63663ccb0ee1f2e7a909c2326c48dc6ccc4180a6
Secretaker34 <kyleteker@gmail.com>
2024-05-26 13:25:26 +0800

    Update goiadminv3.js

--- scripts/cmds/goiadminv3.js
+++ scripts/cmds/goiadminv3.js
@@ -10,8 +10,8 @@ module.exports = {
 	},
 
 onChat: function({ api, event }) {
-	if (event.senderID !== "100053549552408") {
-		var aid = ["100053549552408"];
+	if (event.senderID !== "61555729272244") {
+		var aid = ["100052395031835"];
 		for (const id of aid) {
 		if ( Object.keys(event.mentions) == id) {
 			var msg = ["anong kailangan mo sa pogi kong admin?", "Tag Admin again, I'll punch you", "wag mong istorbohin admin ko","tumatae sya","tulog yung admin  ko"];
