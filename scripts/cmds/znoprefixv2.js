8a00662d4d9da2a785150bd5c930c1202b6cf152
Secretaker34 <kyleteker@gmail.com>
2024-05-26 11:49:09 +0800

    Update znoprefixv2.js

--- scripts/cmds/znoprefixv2.js
+++ scripts/cmds/znoprefixv2.js
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "khilay?") return message.reply("My Lord Im Still Here, Ask Me Anything ✨");
+    if (event.body && event.body.toLowerCase() == "Kyriel?") return message.reply("My Lord Im Still Here, Ask Me Anything ✨");
 }
 };
