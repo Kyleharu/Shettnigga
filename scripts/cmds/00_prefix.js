a961aeb7bee85ce2b284e14f3a8b95e5d7e5e17a
Secretaker34 <kyleteker@gmail.com>
2024-05-26 12:54:18 +0800

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -1,8 +1,11 @@
+const moment = require("moment-timezone");
+const manilaTime = moment.tz('Asia/Manila');
+const formattedDateTime = manilaTime.format('MMMM D, YYYY h:mm A');
 module.exports = {
  config: {
 	 name: "prefix",
 	 version: "1.0",
-	 author: "Tokodori_Frtiz",//remodified by cliff
+	 author: "Tokodori_Frtiz",//remodified by Kyle
 	 countDown: 5,
 	 role: 0,
 	 shortDescription: "no prefix",
@@ -15,13 +18,12 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 + 𓆪 ]\n
-𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
+heyy {userNameTag}, my prefix is [ × ]\n\n📅 | ⏰ 𝗗𝗔𝗧𝗘 𝗔𝗡𝗗 𝗧𝗜𝗠𝗘 :\n${formattedDateTime}\n📌 𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘??:
 ➥ help [number of page] -> see commands
 ➥ sim [message] -> talk to bot
 ➥ callad [message] -> report any problem encountered
-➥ help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/m.zenesha`,
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/jWoZtjO.gif")
+➥ help [command] -> information and usage of command\n\nThank you for using my bot\n\nBot Developer: https://www.facebook.com/itssmekylebaitit`,
+ attachment: await global.utils.getStreamFromURL("\n")
  });
  }
  }
